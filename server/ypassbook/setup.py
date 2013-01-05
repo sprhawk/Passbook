7ef2186145127204f4fddb87b72217a1976e43a9
sprhawk <hongbo@yang.me>
2013-01-05 18:40:38 +0800

    updated setup.py version

--- server/ypassbook/setup.py
+++ server/ypassbook/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = "ypassbook",
     packages = ["ypassbook"],
-    version = "0.1.0",
+    version = "0.1.1",
     description = "Create an Apple passbook",
     author = "Yang Hongbo",
     author_email = "hongbo@yang.me",
